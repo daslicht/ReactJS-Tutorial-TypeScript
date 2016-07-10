e943b7333c907f4206a506a8fc9b57d4f9946a5e
daslicht <daslicht@ansolas.de>
2016-07-10 18:13:31 +0200

    fiexed  props

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,14 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { CommentBox } from "./components/CommentBox/CommentBox";
-import { Hello } from "./components/Hello/Hello";
-
  
-let data = [
-{id: 1, author: "Pete Hunt edited", text: "This is one comment"},
-{id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
+// let data = [
+// {id: 1, author: "Pete Hunt edited", text: "This is one comment"},
+// {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+// ];
 
 ReactDOM.render(
-    <CommentBox data={data}  url="/api/comments" pollInterval={2000}/>,
+    <CommentBox url="/api/comments" pollInterval={2000}/>,
     // <Hello compiler="TypeScript" framework="React" />,
     document.getElementById("example")
 );
